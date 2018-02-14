feae967e9009298056ee280f2d77b6aa433dc821
pascalXzy <z8xiao@ucsd.edu>
2018-02-13 16:11:35 -0800

    dynamically rendering table

--- js/driver-trip-info.js
+++ js/driver-trip-info.js
@@ -45,3 +45,42 @@ var all_trips = [
 ];
 
 localStorage.setItem('driver_all_trips', JSON.stringify(all_trips));
+
+// Get the amount of objects inside 'all_trips' so we can loop through each one.
+var trip_count = all_trips.length;
+var trip_form_keys = Object.keys(all_trips[0]);
+var column_count = trip_form_keys.length;
+
+// Make the results table to include in our output.
+var tableHeader = "<table><thead><tr>" + 
+	        	"<th>Name</th>" + 
+	            "<th>Total Riders</th>" + 
+	            "<th>Pick-up</th>" + 
+	            "<th>Destination</th>" + 
+	            "<th>Flight Number</th>" + 
+	            "<th>Arrival Time</th>" + 
+	            "<th>Nickname</th>" + 
+	            "<th>Status</th>" +
+	        	"</tr></thead><tbody>";
+var tableContent = "";
+
+// Loop through the JSON and output each row in to a string.
+for (i = 0; i < trip_count; i++) {
+	var cur_row = "<tr>";
+	for (j = 0; j < column_count; j++) {
+		cur_row = cur_row + "<td>" + all_trips[i][trip_form_keys[j]] + "</td>";
+	}
+	cur_row = cur_row + '<td><button class="button message">Message</button></td>' + "</tr>";
+	tableContent = tableContent + cur_row;
+}
+
+var tableFooter = "</table>";
+
+// Get div and output the HTML. You can include these HTML strings straight in to your emailText variable.
+window.onload = function(){
+	render_table();
+	function render_table() {
+		document.getElementById("table-render").innerHTML = tableHeader + tableContent + tableFooter;
+	}
+}
+
