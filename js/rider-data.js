d3fc743c7a0708becc31a0f8ee3f6b25a50cf38a
Junru Thomas Ren <Junru.T.Ren@gmail.com>
2018-02-17 03:32:27 -0800

    Created a JS for rider's data

---
+++ js/rider-data.js
@@ -0,0 +1,9 @@
+var user_info_rider = {
+    "first_name": "Ziyi",
+    "last_name": "Xiao",
+    "nick_name": "Annie",
+    "phone": "8581234567",
+    "email": "anniexiao@example.com",
+};
+
+localStorage.setItem('user_info_rider', JSON.stringify(user_info_rider));
