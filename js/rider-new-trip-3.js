5c0b5c862960b3e6acd21a261e76f5373d06167e
Junru Thomas Ren <Junru.T.Ren@gmail.com>
2018-02-17 14:02:09 -0800

    Added JS for rider-new-trip-3

---
+++ js/rider-new-trip-3.js
@@ -0,0 +1,58 @@
+window.onload = function() {
+    // Get all elements needed
+    var check_share_allowed = document.getElementById("check-share-allowed");
+    var check_unknown = document.getElementById("check-unknown");
+    var input_num = document.getElementById("input-num");
+    var input_origin = document.getElementById("input-origin");
+    var input_destination = document.getElementById("input-destination");
+    var next_btn = document.getElementById("next-btn");
+
+    // Read from localStorage
+    var rider_trip_demo = JSON.parse(localStorage.getItem('rider_trip_demo'));
+
+    // Populate the flight info
+    var flight_number = rider_trip_demo['arrival']['flight_info']['flight_number'];
+    var airport = rider_trip_demo['arrival']['flight_info']['airport'];
+    var time = rider_trip_demo['arrival']['flight_info']['time'];
+    var date = rider_trip_demo['arrival']['flight_info']['date'];
+    document.getElementById("flight-number").innerHTML = "Flight " + flight_number;
+    document.getElementById("airport").innerHTML = "For " + airport;
+    document.getElementById("time").innerHTML = time;
+    document.getElementById("date").innerHTML = date;
+
+    next_btn.onclick = function() {
+        // Read user's input
+        var share_allowed = check_share_allowed.checked;
+        var num = input_num.value;
+        var unknown = check_unknown.checked;
+        var origin = input_origin.value;
+        var destination = input_destination.value;
+        // Disallow continuing without an selection
+        if (!origin || !destination) {
+            window.alert("Please tell us about your pickup/dropoff location(s) ψ(｀∇´)ψ");
+            return;
+        }
+        // Write the selection to localStorage
+        var passenger_info = {
+            "total_riders": num,
+            "unknown": unknown,
+        };
+        var locations = {
+            "origin": origin,
+            "destination": destination,
+        };
+
+        rider_trip_demo['arrival']['flight_info']['share_allowed'] = share_allowed;
+        rider_trip_demo['arrival']['passenger_info'] = passenger_info;
+        rider_trip_demo['arrival']['locations'] = locations;
+        localStorage.setItem('rider_trip_demo', JSON.stringify(rider_trip_demo));
+
+        // Jump to next step
+        location.href='rider-new-trip-4.html';
+    };
+};
+
+/*
+ * Stretch Goal: Use Google Maps API for locations
+ * https://developers.google.com/maps/documentation/javascript/examples/places-searchbox
+ */
