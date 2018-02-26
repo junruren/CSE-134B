de84d941b261b9a3c0454c395adb71cc7cd35bfd
JokeyChen <jokey.chen@gmail.com>
2018-02-26 10:41:57 -0500

    Setup react project structure and migrate homepage

---
+++ easyfly/src/Home.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import './css/master.css';
+import './css/index.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <div className="hero">
+          <div className="layer">
+            <h1>EasyFly</h1>
+            <h2>Airport pickup service made easy</h2>
+          </div>
+        </div>
+        <div className="container">
+          <div className="highlights">
+            <div className="feature">
+              <i className="fas fa-plane"></i>
+              <p>Feature 1 text goes here. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <div className="feature">
+              <i className="fas fa-comment-alt"></i>
+              <p>Feature 2 text goes here. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <div className="feature">
+              <i className="fas fa-car"></i>
+              <p>Feature 3 text goes here. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+          </div>
+          <div className="onboard-buttons">
+            <button className="button" onclick="location.href='driver-login.html'">I am a driver</button>
+            <button className="button" onclick="location.href='rider-login.html'">I am a rider</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
