b5dbc7fefdf9e2c39a0ef03d3a15abd175ce388a
JokeyChen <jokey.chen@gmail.com>
2018-03-01 23:54:40 -0800

    Change Login pages to inline styles

--- easyfly/src/Login.js
+++ easyfly/src/Login.js
@@ -12,14 +12,14 @@ const Login = (props) => {
   }
   
   return (
-    <div className="container">
+    <div style={{textAlign: 'center'}}>
       <div className="vertical-middle">
-        <h1 style={{color: 'black'}}>Log In as a {props.usertype}</h1>
+        <h1 style={{color: 'black', fontSize: '2.5em'}}>Log In as a {props.usertype}</h1>
         <div className="signup-form">
-          <input type="email" name="email" placeholder="Email" />
-          <input type="password" name="password" placeholder="Password" />
-          <Link to={loginUrl}><button className="button">Log In</button></Link>
-          <a>Forgot the password?</a>
+          <input style={{...styles.rectangles, ...styles.textInput}} type="email" name="email" placeholder="Email" />
+          <input style={{...styles.rectangles, ...styles.textInput}} type="password" name="password" placeholder="Password" />
+          <Link to={loginUrl} style={{display: 'block'}}><button style={styles.rectangles} className="button">Log In</button></Link>
+          <Link to="/">Forgot the password?</Link>
           <p>Don't have an account? Sign Up</p>
         </div>
       </div>
@@ -34,3 +34,19 @@ export const DriverLogin = () => {
 export const RiderLogin = () => {
   return <Login usertype="Rider" />;
 };
+
+const styles = {
+  rectangles: {
+    width: '60%',
+    height: '50px',
+    margin: '40px auto',
+    display: 'block',
+    border: '1px solid black',
+    borderRadius: '5px',
+    fontSize: '1em'
+  },
+  textInput: {
+    textAlign: 'left',
+    paddingLeft: '10px'
+  }
+}
