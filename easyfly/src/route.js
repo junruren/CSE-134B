98773c23dd32dd0e1497f41c4b1b0fdbe36cfe75
Junru Thomas Ren <Junru.T.Ren@gmail.com>
2018-03-02 10:57:40 -0800

    Fixed indentation

--- easyfly/src/route.js
+++ easyfly/src/route.js
@@ -2,22 +2,22 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 import React from 'react'
 import Home from './Home';
-import {DriverLogin, RiderLogin} from './Login';
+import { DriverLogin, RiderLogin } from './Login';
 import DriverHome from './DriverHome'
 import DriverNewRequests from './DriverNewRequests'
 import DriverAllTrips from './DriverAllTrips'
 
 const Paths = () => (
-  <BrowserRouter>
-  	<div>
-    	<Route exact path="/" component={Home} />
-    	<Route path="/driver_home" component={DriverHome} />
-    	<Route path="/driver_new_requests" component={DriverNewRequests} />
-    	<Route path="/driver_all_trips" component={DriverAllTrips} />
-      <Route path="/driver/login" component={DriverLogin}/>
-      <Route path="/rider/login" component={RiderLogin}/>
- 		</div>
- 	</BrowserRouter>
+	<BrowserRouter>
+		<div>
+			<Route exact path="/" component={Home} />
+			<Route path="/driver_home" component={DriverHome} />
+			<Route path="/driver_new_requests" component={DriverNewRequests} />
+			<Route path="/driver_all_trips" component={DriverAllTrips} />
+			<Route path="/driver/login" component={DriverLogin} />
+			<Route path="/rider/login" component={RiderLogin} />
+		</div>
+	</BrowserRouter>
 );
 
 export default Paths;
