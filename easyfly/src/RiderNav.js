82f567389da3fe6d5e49a6652cc63198d6a4ea21
Junru Thomas Ren <Junru.T.Ren@gmail.com>
2018-03-04 00:39:11 -0800

    Added RiderNav.js

---
+++ easyfly/src/RiderNav.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './css/dashboard.css';
+
+class RiderNav extends Component {
+	state = {
+		user: "rider"
+	};
+
+	render() {
+		return (
+			<ul className="navbar">
+				<li><Link to="/">Sign Out</Link></li>
+				<li className="dropdown">
+					<a href="javascript:void(0)" className="dropbtn">Account</a>
+					<div className="dropdown-content">
+						<Link to={this.state.user + "_all_trips"}>My Trips</Link>
+						<Link to={this.state.user + "_settings"}>Settings</Link>
+					</div>
+				</li>
+				<li><Link to="">Notification</Link></li>
+				<li><Link to="/rider/home">Home</Link></li>
+			</ul>
+		);
+	}
+}
+
+export default RiderNav;
