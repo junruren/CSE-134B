efafac22cc7528ba2d7918f2d1eacedeafb6f802
Junru Thomas Ren <Junru.T.Ren@gmail.com>
2018-03-03 23:53:48 -0800

    Fixed indentation

--- easyfly/src/route.js
+++ easyfly/src/route.js
@@ -2,25 +2,25 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 import React from 'react'
 import Home from './Home';
-import {DriverLogin, RiderLogin} from './Login';
-import {DriverSignup, RiderSignup} from './Signup';
+import { DriverLogin, RiderLogin } from './Login';
+import { DriverSignup, RiderSignup } from './Signup';
 import DriverHome from './DriverHome'
 import DriverNewRequests from './DriverNewRequests'
 import DriverAllTrips from './DriverAllTrips'
 
 const Paths = () => (
-  <BrowserRouter>
-  	<div>
-    	<Route exact path="/" component={Home} />
-    	<Route path="/driver_home" component={DriverHome} />
-    	<Route path="/driver_new_requests" component={DriverNewRequests} />
-    	<Route path="/driver_all_trips" component={DriverAllTrips} />
-      <Route path="/driver/login" component={DriverLogin}/>
-      <Route path="/driver/signup" component={DriverSignup}/>
-      <Route path="/rider/login" component={RiderLogin}/>
-      <Route path="/rider/signup" component={RiderSignup}/>
- 		</div>
- 	</BrowserRouter>
+	<BrowserRouter>
+		<div>
+			<Route exact path="/" component={Home} />
+			<Route path="/driver_home" component={DriverHome} />
+			<Route path="/driver_new_requests" component={DriverNewRequests} />
+			<Route path="/driver_all_trips" component={DriverAllTrips} />
+			<Route path="/driver/login" component={DriverLogin} />
+			<Route path="/driver/signup" component={DriverSignup} />
+			<Route path="/rider/login" component={RiderLogin} />
+			<Route path="/rider/signup" component={RiderSignup} />
+		</div>
+	</BrowserRouter>
 );
 
 export default Paths;
