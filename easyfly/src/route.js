d4d879557d533796e66b6d71c57c1c7995196795
pascalXzy <z8xiao@ucsd.edu>
2018-03-03 23:51:29 -0800

    Added driver settings

--- easyfly/src/route.js
+++ easyfly/src/route.js
@@ -9,6 +9,7 @@ import DriverNewRequests from './DriverNewRequests'
 import DriverAllTrips from './DriverAllTrips'
 import DriverSendQuote from './DriverSendQuote'
 import DriverQuoteSent from './DriverQuoteSent'
+import DriverSettings from './DriverSettings'
 import Chat from './Chat';
 
 const Paths = () => (
@@ -20,6 +21,7 @@ const Paths = () => (
     	<Route path="/driver_all_trips" component={DriverAllTrips} />
       <Route path="/driver_send_quote" component={DriverSendQuote} />
       <Route path="/driver_quote_sent" component={DriverQuoteSent} />
+      <Route path="/driver_settings" component={DriverSettings} />
       <Route path="/driver/login" component={DriverLogin}/>
       <Route path="/driver/signup" component={DriverSignup}/>
       <Route path="/rider/login" component={RiderLogin}/>
