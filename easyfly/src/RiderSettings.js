2e51b9d5c0a8d0d44d08a53ac800036dcfae1f54
Junru Thomas Ren <Junru.T.Ren@gmail.com>
2018-03-04 11:30:48 -0800

    Added line breaks

--- easyfly/src/RiderSettings.js
+++ easyfly/src/RiderSettings.js
@@ -19,12 +19,16 @@ class RiderSettings extends Component {
 		          <div className="settings-info-details">
 		            <label>First Name:</label>
 		            <input type="text" name="firstname" value="Scarlett"  />
+								<br />
 		            <label>Nick Name:</label>
 		            <input type="text" name="nickname" value="Scarlett"  />
+								<br />
 		            <label>Last Name:</label>
 		            <input type="text" name="lastname" value="Honda"  />
+								<br />
 		            <label>Phone Number:</label>
 		            <input type="number" name="phone-number" value="8581234567"  />
+								<br />
 		            <label>Email Address:</label>
 		            <input type="email" name="email" value="scarlett_honda@example.com"  />
 		          </div>
